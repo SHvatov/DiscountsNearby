a4134270721c6b77df18d72ca26e63e6ebfd1e75
MarchenkoVladislav <vlad25091999@mail.ru>
2020-11-19 22:26:15 +0300

    Solution of version conflict

--- src/main/kotlin/com/discounts/nearby/controller/HomeController.kt
+++ src/main/kotlin/com/discounts/nearby/controller/HomeController.kt
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
 /**
- * @author Created by Vladislav Marchenko on 19.11.2020
+ * @author Created by Vladislav Marchenko
  */
 @Controller
 class HomeController {
