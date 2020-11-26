0ccaf44746bd264eb944296f80caf8a48709d19e
MarchenkoVladislav <vlad25091999@mail.ru>
2020-11-26 14:13:15 +0300

    Add location searching with clicking on map

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,9 +1,54 @@
 ymaps.ready(init);
 
 function init() {
-    let myMap = new ymaps.Map("map", {
+    let myPlacemark, myMap = new ymaps.Map("map", {
         center: [55.76, 37.64],
         zoom: 7,
         controls: []
+    }, {
+        searchControlProvider: 'yandex#search'
     });
+
+    myMap.events.add('click', function (e) {
+        let coords = e.get('coords');
+
+        if (myPlacemark) {
+            myPlacemark.geometry.setCoordinates(coords);
+        } else {
+            myPlacemark = createPlacemark(coords);
+            myMap.geoObjects.add(myPlacemark);
+
+            myPlacemark.events.add('dragend', function () {
+                getAddress(myPlacemark.geometry.getCoordinates());
+            });
+        }
+        getAddress(coords);
+        if (myPlacemark.properties._data.balloonContent)
+            $('#location-input').val(myPlacemark.properties._data.balloonContent);
+    });
+
+    function createPlacemark(coords) {
+        return new ymaps.Placemark(coords, {
+            iconCaption: 'поиск...'
+        }, {
+            preset: 'islands#violetDotIconWithCaption',
+            draggable: true
+        });
+    }
+
+    function getAddress(coords) {
+        myPlacemark.properties.set('iconCaption', 'поиск...');
+        ymaps.geocode(coords).then(function (res) {
+            let firstGeoObject = res.geoObjects.get(0);
+
+            myPlacemark.properties
+                .set({
+                    iconCaption: [
+                        firstGeoObject.getLocalities().length ? firstGeoObject.getLocalities() : firstGeoObject.getAdministrativeAreas(),
+                        firstGeoObject.getThoroughfare() || firstGeoObject.getPremise()
+                    ].filter(Boolean).join(', '),
+                    balloonContent: firstGeoObject.getAddressLine()
+                });
+        });
+    }
 }
