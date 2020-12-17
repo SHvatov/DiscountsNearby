ec945a5d4712e1a747869eecc22a2fb7b9d9aa82
Sergey Hvatov <ishvatov@gmail.com>
2020-12-17 21:16:29 +0300

    Add Spring Security support to the project - added spring security and oAuth2 support

---
+++ src/main/kotlin/com/discounts/nearby/config/SecurityConfig.kt
@@ -0,0 +1,54 @@
+package com.discounts.nearby.config
+
+import com.discounts.nearby.model.User
+import com.discounts.nearby.repository.UserRepository
+import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.HttpStatus
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository
+
+
+/**
+ * @author shvatov
+ */
+@Configuration
+@EnableWebSecurity
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests {
+            it.apply {
+                antMatchers(*ALLOWED_ENDPOINTS).permitAll()
+                    .anyRequest().authenticated()
+            }
+        }.exceptionHandling {
+            it.authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+        }.logout {
+            it.logoutSuccessUrl("/").permitAll()
+        }.csrf {
+            it.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+        }.oauth2Login()
+    }
+
+    @Bean
+    fun principalExtractor(usersRepository: UserRepository) = PrincipalExtractor {
+        val id = it["sub"] as String
+        val user = usersRepository.findByIdOrNull(id)
+            ?: User().apply {
+                this.id = id
+                this.email = it["email"] as? String
+                this.name = it["name"] as? String
+            }
+        usersRepository.save(user)
+    }
+
+    private companion object {
+        val ALLOWED_ENDPOINTS = arrayOf("/", "/error")
+    }
+}
