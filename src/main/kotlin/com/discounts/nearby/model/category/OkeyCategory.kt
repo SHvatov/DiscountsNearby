cbf4b18a6cc01b5d587d2c165000419d3b635b00
Kopeyka885 <ccadyrov@yandex.ru>
2020-12-22 09:29:14 +0300

    Add new category to the enum

--- src/main/kotlin/com/discounts/nearby/model/category/OkeyCategory.kt
+++ src/main/kotlin/com/discounts/nearby/model/category/OkeyCategory.kt
@@ -7,5 +7,6 @@ enum class OkeyCategory(
     override val goodCategory: GoodCategory,
     override val localizedCategory: String
 ) : SupermarketCategory {
-    BEER(GoodCategory.BEER, "alkogol-nye-napitki/pivo")
+    BEER(GoodCategory.BEER, "alkogol-nye-napitki/pivo"),
+    MEET(GoodCategory.MEET, "miaso-ptitsa-kolbasy/miaso-i-ptitsa")
 }
