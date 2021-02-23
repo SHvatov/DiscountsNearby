b613ffc626871c3495b5ce2d1d1e28c292dda143
MarchenkoVladislav <vlad25091999@mail.ru>
2021-02-23 16:26:44 +0300

    Categories renaming

--- src/main/kotlin/com/discounts/nearby/model/category/LentaCategory.kt
+++ src/main/kotlin/com/discounts/nearby/model/category/LentaCategory.kt
@@ -8,5 +8,5 @@ enum class LentaCategory(
     override val localizedCategory: String
 ) : SupermarketCategory {
     BEER(GoodCategory.BEER, "alkogolnye-napitki"),
-    MEET(GoodCategory.MEAT, "myaso-ptica-kolbasa")
+    MEAT(GoodCategory.MEAT, "myaso-ptica-kolbasa")
 }
--- src/main/kotlin/com/discounts/nearby/model/category/OkeyCategory.kt
+++ src/main/kotlin/com/discounts/nearby/model/category/OkeyCategory.kt
@@ -8,5 +8,5 @@ enum class OkeyCategory(
     override val localizedCategory: String
 ) : SupermarketCategory {
     BEER(GoodCategory.BEER, "alkogol-nye-napitki/pivo"),
-    MEET(GoodCategory.MEAT, "miaso-ptitsa-kolbasy/miaso-i-ptitsa")
+    MEAT(GoodCategory.MEAT, "miaso-ptitsa-kolbasy/miaso-i-ptitsa")
 }
